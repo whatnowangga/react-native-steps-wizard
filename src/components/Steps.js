0e99a05e7b9444420b45aaca9715efa6af02bfcf
Angga Andrean <anggaand123@gmail.com>
2020-01-16 22:52:23 +0700

    Update Wrong Path

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './reducers';
+import reducers from './../reducers/';
 import StepWrap from './stepbar/StepWrap';
 
 class Steps extends Component {
